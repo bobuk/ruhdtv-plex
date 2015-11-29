eb4bf1b76300e41c445533b08aed81faa319e1db
Grigory Bakunov <bobuk@yandex-team.ru>
2015-11-29 04:13:07 +0300

    well, i dont know. looks like it works

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -6,11 +6,10 @@
 
 import re
 from copy import deepcopy
-####################################################################################################
 
+VERSION = 1.4
 
-
-VIDEO_PREFIX = "/video/hdouttv"
+PREFIX = "/video/hdouttv"
 
 SITE = "http://hdout.tv/"
 S_SERIES = SITE + "List/"
@@ -22,276 +21,172 @@ S_FULLPATH = SITE + 'EpisodeLink/'
 S_FULLPATH_XML = S_FULLPATH + '%s/XML/'
 S_RSS_PATH = SITE + "RSS/"
 NAME = L('Title')
+TITLE = "HDOut.tv"
 
-ART           = 'art-tv.png'
-ICON          = 'icon-tv.png'
+ART           = 'art.png'
+ICON          = 'icon.png'
 
 ####################################################################################################
 
 authed = False
+token  = False
 
 def Start():
-    Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, L('VideoTitle'), ICON, ART)
-    Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-    Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+    ObjectContainer.art = R(ART)
+    ObjectContainer.title1 = TITLE
+    DirectoryObject.thumb = R(ICON)
 
-    MediaContainer.art = R(ART)
-    MediaContainer.title1 = NAME
-    DirectoryItem.thumb = R(ICON)
+    HTTP.CacheTime = CACHE_1HOUR
 
-def CreatePrefs():
-    Prefs.Add(id='username', type='text', default='', label='Username')
-    Prefs.Add(id='password', type='text', default='', label='Password', option='hidden')
 
-def ValidatePrefs(suspendOk=False):
+def ValidatePrefs():
     u = Prefs['username']
     p = Prefs['password']
     if( u and p ):
-        return True if Authentificate(u, p) else MessageContainer("Error", "Wrong username or password")
+        return True if Authentificate() else MessageContainer("Error", "Wrong username or password")
     else:
         return MessageContainer( "Error",   "You need to provide both a user and password" )
 
-def Authentificate(user, passwd):
-    global authed
-    if authed:
+def Authentificate():
+    if Prefs['username'] and Prefs['password']:
+        req = HTTP.Request(SITE, values = {
+            'login' : Prefs["username"],
+            'password' : Prefs["password"]}, cacheTime=CACHE_1WEEK)
+        if 'search' not in str(req.content):
+            Log("Oooops, wrong pass or no creds")
+            return False
+        else:
+            Log("Ok, i'm in!")
+            cookies = HTTP.CookiesForURL(SITE)
+            if cookies != None:
+                Dict['Cookies'] = cookies
+                Dict['SID'] = cookies.split('=', 1)[1]
+                authed = True
+            else:
+                Log('Cached: ' + Dict['Cookies'])
         return True
-    req = HTTP.Request(SITE, values={"login": user, "password": passwd})
-    if "search" not in str(req):
-        Log("Oooops, wrong pass or no creds")
-        return False
-    Log("Ok, i'm in!")
-    authed = True
-    return True
-
-def FetchXML(url):
-    try:
-        xml = XML.ElementFromURL(url)
-    except:
-        Log('Need auth or bad xml')
-        xml = None
-    if xml is None or str(xml.tag) != 'document':
-        f = Authentificate(Prefs['username'], Prefs['password'])
-        if not f:
-            return None
-        xml = XML.ElementFromURL(url)
-    return xml
-
-def noHTML(text):
-    return text.replace('<br />','\n').replace('<br/>', '\n')\
-               .replace('&ndash;',u'\u2013')\
-               .replace('&mdash;',u'\u2014')\
-               .replace('&raquo;','»').replace('&laquo;', '«')\
-               .replace('&quot;', '"')\
-               .replace('&hellip;', '…')\
-               .replace('<span style="font-weight: bold;">', '«')\
-               .replace('</span>', '»') if text else text
-               
-
-def m_ig(xml, tid, default = None):
-    try:
-        return xml.xpath(tid)[0].text
-    except:
-        return default
-
-class Video:
-    def __init__(self, ids, xml):
-        xml = xml.xpath('//item')[0]
-        self.ids = ids
-        self.snd = [m_ig(xml,'./defsnd'), m_ig(xml, './addsnd')]
-        self.sub = [m_ig(xml,'./sub1'),   m_ig(xml,'./sub2')   ]
-        self.url = m_ig(xml, './videourl')
-
-class Episode:
-    def __init__(self, mark, xml):
-        self.mark   = mark
-        self.title  = m_ig(xml, './title')
-        self.etitle = m_ig(xml, './etitle')
-        if not self.title or len(self.title) < 1:
-            self.title = self.etitle
-        self.type   = m_ig(xml, './type')
-        self.server   = m_ig(xml, './server')
-        self.info   = noHTML(m_ig(xml, './info'))
-        self.ids    = m_ig(xml, './id_episodes')
-        self.snum   = int(m_ig(xml, './snum'))
-        self.enum   = int(m_ig(xml, './enum'))
-        self.vnum   = m_ig(xml, './vnum')
-        if self.type == 1:
-            self.thumb = SITE + "v/1/sd/%s/%02d-%02d.jpg" % (self.mark, self.snum, self.enum)
-        else:
-            self.thumb = SITE + "v/1/hd/%s/sc/%02d-%02d.jpg" % (self.mark, self.snum, self.enum)
+    Log('No prefs')
+    return False
 
 
-    def __repr__(self):
-        return '%s > %s s%de%s (%s)' % ( self.mark, self.title, self.snum, self.vnum, self.ids)
+@handler(PREFIX, TITLE, thumb=ICON, art=ART)
+def MainMenu():
+
+    oc = ObjectContainer()
+    oc.add(DirectoryObject(key=Callback(Serials, title2='List of all shows on this site', filter='all'), title='All shows'))
+    oc.add(DirectoryObject(key=Callback(Serials, title2='List of your favorite shows', filter='favorites'), title='Favorites'))
+    oc.add(PrefsObject(title="Preferences"))
+    oc.add(DirectoryObject(key=Callback(Serials, title2='List of new or updated series', filter='updated'), title='Updates'))
+
+    return oc
+
+
+@route(PREFIX+'/serials/{filter}')
+def Serials(title2, filter):
+    if not Authentificate():
+        return MessageContainer( "Error",   "You need to provide both a user and password" )
+
+    dir = ObjectContainer(title2=title2)
     
-    def __str__(self):
-        return self.__repr__()
+    if filter == 'all':
+        xml = XML.ElementFromURL(S_SERIES_XML)
+    elif filter == 'favorites':
+        xml = XML.ElementFromURL(S_MY_SERIES_XML)
 
-class Serial:
-    def __init__(self, xml):
-        self.title  = m_ig(xml, './title')
-        self.etitle = m_ig(xml, './etitle')
-        if not self.title or len(self.title) < 1:
-            self.title = self.etitle
-        self.info   = noHTML( m_ig(xml, './info') )
-        self.ids    = m_ig(xml, './id_series')
-        self.mark   = m_ig(xml, './mark')
-        self.type   = m_ig(xml, './type')
-        if self.type == '1':
-            self.thumb  = SITE + "static/c/sd/s/" + self.mark + '.jpg'
-            self.art    = SITE + "static/c/sd/b/" + self.mark + '.jpg'
-            self.title  = self.title + ' [SD]'
-        else:
-            self.thumb  = SITE + "static/c/hd/s/" + self.mark + '.jpg'
-            self.art    = SITE + "static/c/hd/b/" + self.mark + '.jpg'
-            self.title  = self.title + ' [HD]'
+    if filter in ['all', 'favorites']:
+        for item in xml.xpath('//serieslist/item'):
+            title = item.xpath('./title')[0].text
+            summary = item.xpath('./info')[0].text
+            mark = item.xpath('./mark')[0].text
 
-    def __repr__(self):
-        return '%s/%s (%s/%s)' % ( self.title, self.etitle, self.ids, self.mark)
+            poster = SITE + "static/c/b/" + mark + ".jpg"
+            thumb = SITE + "static/c/s/" + mark + ".jpg"
 
-    def __str__(self):
-        return self.__repr__()
-
-def FetchSeriesList(favs=False):
-    sv = {}
-    serieslist = []
-    favorites  = []
-    xml = FetchXML(S_SERIES_XML)
-    if xml is None:
-        return None
-    for item in xml.xpath('//document/fp/serieslist/item'):
-        serial = Serial(item)
-        serieslist.append(serial)
-        sv[serial.ids] = serial
-    serieslist.sort(lambda x,y: -1 if x.title < y.title else 1)
-    if favs:
-        for item in xml.xpath('//document/favorites/item/series'):
-            try:
-                favorites.append(sv[item.text])
-            except:
-                Log('No items for Serial ' + item.text)
-        return favorites
-    return serieslist
-
-def FetchRSSEpisodesList():
-    rsslist = []
-    xml = RSS.FeedFromURL(S_RSS_PATH)
-    if xml is None:
-        return None
-    for item in xml.entries:
-        ids = item.link.split('/')[4]
-        rsslist.append((ids, item.title))
-    return rsslist
-    
-def FetchEpisodesList(ids, mark):
-    episodeslist = []
-    xml = FetchXML(S_EPISODES_XML % ids)
-    if xml is None:
-        return None
-    for item in xml.xpath('/document/series/season/item'):
-        episodeslist.append(Episode(mark, item))
-    return episodeslist
-
-def FetchVideoItem(ids):
-    xml = FetchXML(S_FULLPATH_XML % ids)
-    if xml is None:
-        return None
-    return Video(ids, xml)
-
-def Series(sender, ids, mark, title, art):
-    mc = MediaContainer()
-    series = FetchEpisodesList(ids, mark)
-    if series is None:
-        return MessageContainer("Error", "Can't do that.\nCheck preferences or refill your ballance!")
-    for seria in series:
-        mc.Append(
-            Function(
-                VideoItem(
-                    Videos,
-                    "%d.%d. %s" %(seria.snum, seria.enum, seria.title),
-                    art = art,
-                    thumb = seria.thumb
-                    ),
-                ids = seria.ids))
-    return mc
-
-def Videos(sender, ids):
-    v = FetchVideoItem(ids)
-    Log('Video: ')
-    if v is None:
-        return MessageContainer("Error", "Can't do that.\nCheck preferences or refill your ballance!")
-    Log(v.url)
-    parts = [ PartObject(key = v.url) ]
-    # return MediaObject(parts = parts)
-    # PartObject(Redirect( url = v.url))
-    return Redirect( url = v.url)
-
-def Serials(sender, favs = False):
-    mc = MediaContainer(viewGroup="InfoList")
-    serials = FetchSeriesList(favs)
-    if serials is None:
-        return MessageContainer("Error", "Can't do that.\nCheck preferences or refill your ballance!")
-    for item in serials:
-        mc.Append(
-            Function(
-                DirectoryItem(
-                    Series,
-                    title = item.title,
-                    subtitle = item.etitle,
-                    summary = item.info,
-                    thumb = item.thumb,
-                    art = item.art),
-                ids  = item.ids,
-                mark = item.mark,
-                title = SITE + item.title,
-                art = SITE + item.art))
-    return mc
-
-def Updates(sender):
-    mc = MediaContainer()
-    series = FetchRSSEpisodesList()
-    if series is None:
-        return MessageContainer("Error", "Can't do that.\nCheck preferences or refill your ballance!")
-    for (ids, title) in series:
-        mc.Append(
-            Function(
-                VideoItem(
-                    Videos,
-                    title,
-                    ),
-                ids = ids))
-    return mc
-    
-
-def VideoMainMenu():
-    dir = MediaContainer(viewGroup="InfoList")
-    if Authentificate(Prefs['username'], Prefs['password']):
-        dir.Append(
-            Function(
-                DirectoryItem(Serials,
-                          title='All serials',
-                          subtitle = 'List of all shows on this site',
-                          thumb=R(ICON)),
-                favs = False))
-        dir.Append(
-            Function(
-                DirectoryItem(Serials,
-                              title='Favorites',
-                              subtitle = 'List of your favorite shows',
-                              thumb=R(ICON)),
-                favs = True))
-        dir.Append(Function(
-                DirectoryItem(Updates,
-                              title = "Updates",
-                              subtitle = 'List of new or updated series',
-                              thumb = R(ICON)
-                )))
+            sid   = item.xpath('./id_series')[0].text
+            dir.add(TVShowObject(key=Callback(
+                                                Serial,
+                                                sid = sid,
+                                                title = title, 
+                                                mark = mark
+                                            ),
+                                 rating_key = sid,
+                                 title = title, 
+                                 summary = summary, 
+                                 art = poster, thumb = thumb))
+        return dir
     else:
-        Log('No auth!')
-    dir.Append(
-        PrefsItem(
-            title="Preferences",
-            subtile="So you can set preferences",
-            summary="lets you set preferences",
-            thumb=R(ICON)))
+        rss = XML.ElementFromURL(S_RSS_PATH, headers = {'Cookie': Dict['Cookies']})
+        for item in rss.xpath('//item'):
+            title = item.xpath('./title')[0].text.strip()
+            thumb = item.xpath('./tumbnail')[0].text
+            Log(thumb)
+            eid = item.xpath('./link')[0].text.split('/')[4]
+            dir.add(EpisodeObject(
+                key=Callback(Episode, eid = eid),
+                rating_key='hdouttv' + '.' + str(eid),
+                title=title,
+                thumb=thumb,
+                items=[
+                    MediaObject(
+                        parts=[
+                            PartObject(
+                                key=Callback(Episode, eid=eid))])]
+                ))
+        return dir
+
+
+@route(PREFIX+'/serial/{sid}')
+def Serial(sid, title, mark):
+
+    dir = ObjectContainer()
+    HTTP.Headers['Cookie'] = Dict['Cookies']
+    seasons = XML.ElementFromURL(S_EPISODES_XML % sid, cacheTime=0)
+
+    for season in seasons.xpath('//season'):
+        for item in season.xpath('./item'):
+            season_num = int(item.xpath('./snum')[0].text)
+            episode_num = int(item.xpath('./enum')[0].text)
+            title = item.xpath('./title')[0].text
+            eid = item.xpath('./id_episodes')[0].text
+            server = item.xpath('./server')[0].text
+            full_title = "%02d-%02d. %s" % (season_num, episode_num, title)
+            thumb=SITE + ("hd/%s/sc/%02d-%02d.jpg" % (mark, season_num, episode_num))
+            dir.add(EpisodeObject(
+                key=Callback(Episode, eid = eid),
+                rating_key='hdouttv' + '.' + str(eid),
+                title=full_title,
+                thumb=thumb,
+                items=[
+                    MediaObject(
+                        parts=[
+                            PartObject(
+                                key=Callback(episode_url, eid=eid))])]
+                ))
+
     return dir
+
+@route(PREFIX+'/episode/{eid}')
+def Episode(eid):
+    oc = ObjectContainer()
+    oc.add(EpisodeObject(
+        key=Callback(Episode, eid = eid),
+        rating_key='hdouttv' + '.' + str(eid),
+        items=[MediaObject(
+            # video_resolution = 720 if row['quality'].encode('utf-8')=='720p' else 400,
+            video_codec = VideoCodec.H264,
+            audio_codec = AudioCodec.AAC,
+            container = Container.MP4,
+            optimized_for_streaming = True,
+            audio_channels = 2,
+            parts = [PartObject(key=Callback(episode_url, eid=eid))]
+        )]
+    ))
+    return oc
+
+def episode_url(eid):
+    Log(S_FULLPATH_XML % eid)
+    HTTP.Headers['Cookie'] = Dict['Cookies']
+    video = XML.ElementFromURL(S_FULLPATH_XML % eid, cacheTime=0)
+    video_url = video.xpath("//videourl")[0].text
+    return Redirect(video_url)
+
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-B="$HOME/Library/Application Support/Plex Media Server/Plug-ins/HDOut.tv.bundle"
-test -d "$B" || B="$HOME/Library/Application Support/Plex Media Server/Plug-ins/HDOut.tv.bundle"
+B="$HOME/Library/Application Support/Plex Media Server/Plug-ins/hdouttv.bundle"
+test -d "$B" || B="$HOME/Library/Application Support/Plex Media Server/Plug-ins/hdouttv.bundle"
 I="$B/Contents"
 mkdir -p "$B/"
 cp -r "Contents" "$B/"
